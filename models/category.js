73395e7e135f1312fdf08374ebdabbe7d6fcbc2c
talberts <trinidad.albert98@gmail.com>
2025-05-30 16:00:36 +0200

    Creadas migraciones product y category

---
+++ models/category.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Category.belongsToMany(models.Product, {
+        through: models.ProductCategory,
+        foreignKey: "categoryId",
+        as: "Products"
+      })
+    }
+  }
+  Category.init({
+    nameCategory: DataTypes.STRING,
+    descriptionCategory: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+  });
+  return Category;
+};
