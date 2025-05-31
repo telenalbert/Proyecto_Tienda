43296ee0da1981ec62367b1087cba7159ce142f0
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-05-31 20:46:59 +0200

    seeder created

---
+++ seeders/20250531183313-demo-product.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.bulkInsert('Products', [
+      {
+        nameProduct: 'Demo-Product 1:The Legend of Zelda',
+        descriptionProduct: 'Juego de aventuras y exploración en un mundo abierto',
+        price: 59.99,
+        orderId: 1, // Asegúrate de que esta Order exista
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nameProduct: 'Demo-Product 2:Super Mario Odyssey',
+        descriptionProduct: 'Plataformas en 3D con exploración y puzzles',
+        price: 49.99,
+        orderId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nameProduct: 'Demo-Product 3:Hades',
+        descriptionProduct: 'Roguelike de acción con historia dinámica',
+        price: 24.99,
+        orderId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nameProduct: 'Demo-Product 4:Stardew Valley',
+        descriptionProduct: 'Simulador de granja con exploración y relaciones',
+        price: 14.99,
+        orderId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nameProduct: 'Demo-Product 5:Celeste',
+        descriptionProduct: 'Plataformas desafiante con gran narrativa',
+        price: 19.99,
+        orderId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Products', null, {});
+  }
+};
